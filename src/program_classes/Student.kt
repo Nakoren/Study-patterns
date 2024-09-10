94bed3c41b809c367a5dad40fc87470ac31522a4
Nyakoren <minenkov_vladimir2003@inbox.ru>
2024-09-10 16:00:30 +0300

    Lab 1. Task 8. Added functions validate, checkGit and checkTelegram to check existing of at least one contact field

--- src/program_classes/Student.kt
+++ src/program_classes/Student.kt
@@ -10,37 +10,30 @@ class Student(
     git: String? = null
     ) {
     var name: String = name
-        get() = field
         set(value) {
             field = value.lowercase()
         }
     var fam_name: String = fam_name
-        get() = field
         set(value) {
             field = value.lowercase()
         }
     var father_name: String = father_name
-        get() = field
         set(value) {
             field = value.lowercase()
         }
     var phone: String? = phone
-        get() = field
         set(value) {
             field = value?.lowercase()
         }
     var email: String? = email
-        get() = field
         set(value) {
             field = value?.lowercase()
         }
     var git: String? = git
-        get() = field
         set(value) {
             field = value?.lowercase()
         }
     var telegram: String? = telegram
-        get() = field
         set(value) {
             field = value?.lowercase()
         }
@@ -48,5 +41,14 @@ class Student(
     override fun toString(): String {
         val str = "Full name: $fam_name $name $father_name,\nphone: $phone \nemail:$email \ngit: $git \ntelegram: $telegram\n"
         return str
+    }
+    fun validate(): Boolean{
+        return checkTelegram() || checkGit()
+    }
+    fun checkGit(): Boolean{
+        return (git != null)&&(git != "")
+    }
+    fun checkTelegram(): Boolean{
+        return (telegram != null)&&(telegram != "")
     }
 }
