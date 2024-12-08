c93184717b69a9b1bc821c2156b0931306d7c7e7
Nakoren <minenkov_vladimir2003@inbox.ru>
2024-12-09 02:56:46 +0300

    Lab 2. Task 2.4
    Added methods

--- src/program_classes/DataList.kt
+++ src/program_classes/DataList.kt
@@ -2,8 +2,25 @@ package program_classes
 
 class DataList(
     data: List<Any>,
-    selected: List<Int>
 ) {
     private val data: List<Any> = data
 
+    private var selected: MutableList<Int> = mutableListOf<Int>()
+
+    fun select(number: Int){
+        if(number<data.size)
+        selected.addLast(number)
+    }
+
+    fun resetSelected(){
+        selected = mutableListOf<Int>()
+    }
+
+    fun getNames(): List<String>{
+        return mutableListOf<String>()
+    }
+
+    fun getData(): DataTable{
+        return DataTable(mutableListOf<List<Any>>())
+    }
 }
